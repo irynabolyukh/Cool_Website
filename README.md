1b5f96ef7a280960446f9340b14f4fd51ae2bc09
irynabolyukh <iryna.boliukh@ukma.edu.ua>
2020-11-23 16:23:57 +0200

    cute favicons added

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 * run **_npm install_** to install all dependencies
 * run **_npm start_** to start the server
-* go to http://localhost:3000/admin
+* go to http://localhost:3000/admin for admin interface
+* go to http://localhost:3000/home for client interface
--- app.js
+++ app.js
@@ -23,7 +23,6 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
 
-
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
