e60987786f6fd366f770fa3101fb6a2e192b07f2
irynabolyukh <iryna.boliukh@ukma.edu.ua>
2020-11-24 11:18:19 +0200

    update applications on admin page

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -61,6 +61,24 @@ router.get('/', (req,res,next) => {
     });
 });
 
+router.get('/applications', (req,res,next) => {
+    Application.find().exec().then(doc => {
+        console.log(doc);
+        if(doc){
+            res.status(200).json(doc);
+        }else{
+            res.status(404).json({
+                message: "No applications found"
+            });
+        }
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+});
+
 router.get('/trainings', (req,res,next) => {
         Training.find().exec().then(doc => {
             console.log(doc);
