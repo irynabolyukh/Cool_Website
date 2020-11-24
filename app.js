bdf532b547ea056bf4cfb5345d38dd7c084a0a29
irynabolyukh <iryna.boliukh@ukma.edu.ua>
2020-11-24 11:41:07 +0200

    config improved

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ const config = require('config');
 const title = config.get('App.title');
 const color = config.get('App.color');
 const domain = config.get('App.domain');
+const image = config.get('App.image');
 const emailVerify = config.get('App.emailVerify');
+const pages = config.get("App.pages");
+const pagesUkr = config.get("App.pages-ukr");
 
 const adminRoutes = require('./api/routes/admin');
 
@@ -38,20 +41,17 @@ app.use((req, res, next) => {
     next();
 });
 
-let pages = [{"name": "HOME", "href": "/home"}, {"name": "TRAININGS", "href": "/trainings"}, {"name": "ABOUT", "href": "/about"}];
-let pagesUkr = [{"name": "ГОЛОВНА", "href": "/home-ukr"}, {"name": "ТРЕНІНГИ", "href": "/trainings-ukr"}, {"name": "ПРО НАС", "href": "/about-ukr"}];
-
 app.use('/home', (req, res) => {
     res.render('home', {
         layout: './layouts/regular-layout', emailVerify: emailVerify,
-        title: title, color: color, pages: pages, domain: domain
+        title: title, color: color, pages: pages, domain: domain, image: image
     });
 });
 
 app.use('/home-ukr', (req, res) => {
     res.render('home-ukr', {
         layout: './layouts/regular-layout-ukr', emailVerify: emailVerify,
-        title: title, color: color, pages: pagesUkr, domain: domain
+        title: title, color: color, pages: pagesUkr, domain: domain, image: image
     });
 });
 
